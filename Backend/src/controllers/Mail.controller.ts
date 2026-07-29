8e149b5df30d8274a34009abd538ab8036cb5160
RohitMunda07 <rohitkumar14818@gmail.com>
2026-07-29 23:31:25 +0530

    feat: add Resend API integration

--- Backend/src/controllers/Mail.controller.ts
+++ Backend/src/controllers/Mail.controller.ts
@@ -3,9 +3,50 @@ import { asyncHandler } from "../utils/asyncHandler";
 import ApiResponse from "../utils/apiResponse";
 import ApiError from "../utils/apiError";
 import { MailZodSchema } from "../validators/mail.schema";
+import mongoose from "mongoose";
 
 const getMails = asyncHandler(async (req, res) => {
-    const { filter } = req.query as { filter?: string }
+    const filter = req.user?.role === 'admin' ? {} : { user: req.user?._id }
 
-    const mails = await MailModel.find(filter)
+    const emails = await MailModel.find(filter);
+    if (emails.length === 0) {
+        throw new ApiError(404, "No Emails found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, emails, "Emails fetched successfully")
+        )
 })
+
+const getMailById = asyncHandler(async (req, res) => {
+    const { mail } = req.params as { mail: string }
+    if (!mongoose.isValidObjectId(mail)) {
+        throw new ApiError(400, "invalid mail Id")
+    }
+
+    const existingMail = await MailModel.findById(mail);
+    if (!existingMail) {
+        throw new ApiError(404, "No mail found")
+    }
+
+    if (req.user?.role !== 'admin' && existingMail.user.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "Forbidden", [], "")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, existingMail, "Mail fetched successfully")
+        )
+})
+
+const resendMail = asyncHandler(async (req, res) => {
+    
+})
+
+export {
+    getMails,
+    getMailById
+}
