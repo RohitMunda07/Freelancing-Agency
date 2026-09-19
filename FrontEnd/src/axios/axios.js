d304357e6cebfbd3b556451ebdd9b1319e0608e1
RohitMunda07 <rohitkumar14818@gmail.com>
2026-09-20 01:08:32 +0530

    Fix increased timeout

--- FrontEnd/src/axios/axios.js
+++ FrontEnd/src/axios/axios.js
@@ -6,7 +6,7 @@ import axios from "axios"
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 const api = axios.create({
     baseURL: baseUrl,
-    timeout: 10000,
+    timeout: 20000,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
